313a429f58dd26bdbf4e51783d95d138d7c59ee9
steve zhang <871506263@qq.com>
2021-03-24 13:07:35 +0800

    提交代码于: 2021-03-24 13:07:35

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -8,7 +8,7 @@ const user = {
     //存储定义的变量
     token: '',
     devServer: 'dev', //生产: "prod" || uat+dev是"dev"
-    isDebugMode: false //写死会员ID为 true || 原生localStorage获取会员ID为 false
+    isDebugMode: true //写死会员ID为 true || 原生localStorage获取会员ID为 false
   },
   mutations: {
     //更改state里面的值定义的一些方法
